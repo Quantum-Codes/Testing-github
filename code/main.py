ec9ed17acb862be9ff93cf2d69423ac8bb022934
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-01-27 17:34:58 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,1 +1,6 @@
 print("hello")
+
+from ScraGet import ScraGet #import package
+user = ScraGet.get_user() #create object
+user.updateScratch("griffpatch") #update data
+print(user.id) #print required info from data*
