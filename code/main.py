975009878f6415b11af70bd170b665d9cd5e47bc
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-01-27 18:53:09 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -3,4 +3,7 @@ print("hello")
 from ScraGet import ScraGet #import package
 user = ScraGet.get_user() #create object
 user.updateScratch("griffpatch") #update data
-print(user.id) #print required info from data*
+print(f"Scraget works: {user.id}") #print required info from data*
+
+with open("test.txt", "a") as file:
+  file.write("testing. look at line#\n")
